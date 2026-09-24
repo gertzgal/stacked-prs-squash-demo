0b71239a2fc65f9acf3a7bd7064d56e00a85539c
gertzgal <gal@clutch.security>
2026-09-24 15:32:51 +0300

    B2: add price_with_discount()

--- showcase_manual.py
+++ showcase_manual.py
@@ -4,3 +4,7 @@ TAX_RATE = 0.18  # layer B: new rate
 
 def price(amount):
     return round(amount * (1 + TAX_RATE), 2)
+
+
+def price_with_discount(amount, pct):
+    return price(amount * (1 - pct))
