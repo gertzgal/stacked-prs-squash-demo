053b4b93c1c36ccfa8927bb69025ca97133d98e5
gertzgal <gal@clutch.security>
2026-09-24 16:11:40 +0300

    Support percentage discounts

--- cart.py
+++ cart.py
@@ -1,6 +1,7 @@
 TAX_RATE = 0.17
 
 
-def total(items):
+def total(items, discount_pct=0):
     subtotal = sum(i["price"] for i in items)
+    subtotal *= 1 - discount_pct
     return round(subtotal * (1 + TAX_RATE), 2)
